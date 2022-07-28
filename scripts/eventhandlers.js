2a1c8533526d16a6813dae57297361aa26011b6c
zumbuul <melje1delje@gmail.com>
2022-07-28 16:16:34 +0200

    finished gallery page animations

--- scripts/eventhandlers.js
+++ scripts/eventhandlers.js
@@ -7,17 +7,23 @@ const images = document.getElementsByClassName("opacity-30");
 window.addEventListener("scroll", () => {
   verticalScroll = window.scrollY;
   if (verticalScroll > 0) {
-    console.log(images);
-    document.querySelector("#title").style.display = "none";
-    document.querySelector("header").style.display = "none";
+    document.querySelector("header").classList.add("urke");
+    document.querySelector("header").classList.remove("velja");
+    document.querySelector("#title").classList.add("urke");
+    document.querySelector("#title").classList.remove("velja");
     for (item of images) {
-      item.style.opacity = "1";
+      item.classList.remove("odzi");
+      item.classList.add("japa");
     }
   } else {
-    document.querySelector("#title").style.display = "flex";
-    document.querySelector("header").style.display = "block";
+    document.querySelector("header").classList.remove("urke");
+    document.querySelector("header").classList.add("velja");
+    document.querySelector("#title").classList.add("velja");
+    document.querySelector("#title").classList.remove("urke");
+
     for (item of images) {
-      item.style.opacity = "0.3";
+      item.classList.add("odzi");
+      item.classList.remove("japa");
     }
   }
 });
